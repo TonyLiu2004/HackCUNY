57957a668ca890236a421ab80693d80597ab2379
TonyLiu <ltsplice@gmail.com>
2024-02-16 13:47:20 -0500

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Navbar from './assets/Navbar';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Navbar />
+      <h1>NYC Event Map</h1>
+    </div>
+  )
+}
+
+export default App
