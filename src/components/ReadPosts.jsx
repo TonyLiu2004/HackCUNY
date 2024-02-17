cb9f2bc2e979fc2d8cba5baed7981c99ebc02be7
Zhi Huang Li <zli8362@bths.edu>
2024-02-17 12:15:55 -0500

    fetch event posts

--- src/components/ReadPosts.jsx
+++ src/components/ReadPosts.jsx
@@ -1,31 +1,37 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { collection, getDocs } from "firebase/firestore";
 import { database } from "../firebase";
+import PostCard from "./PostCard";
 
 function ReadPost() {
-    useEffect(() => {
-        const fetchPosts = async () => {
-            try {
-                // Query 'posts' collection
-                const querySnapshot = await getDocs(collection(database, "posts"));
-                
-                // Log each post
-                querySnapshot.forEach((doc) => {
-                    console.log(doc.id, " => ", doc.data());
-                });
-            } catch (error) {
-                console.error("Error fetching posts: ", error);
-            }
-        };
+  const [posts, setPosts] = useState([]);
 
-        fetchPosts();
-    }, []);
+  useEffect(() => {
+    const fetchPosts = async () => {
+      try {
+        const querySnapshot = await getDocs(collection(database, "posts"));
+        const postData = querySnapshot.docs.map(doc => ({
+          id: doc.id,
+          ...doc.data()
+        }));
 
-    return (
-        <div>
-            
-        </div>
-    );
+        setPosts(postData);
+      } catch (error) {
+        console.error("Error fetching posts: ", error);
+      }
+    };
+
+    fetchPosts();
+  }, []);
+
+  return (
+    <div>
+      <h2>Posts</h2>
+      {posts.map(post => (
+        <PostCard key={post.id} post={post} />
+      ))}
+    </div>
+  );
 }
 
 export default ReadPost;
