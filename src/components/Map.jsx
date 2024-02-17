b432c5f69000c821d20b5a17d4d76ea827e9dcad
Tonyliu2004 <ltsplice@gmail.com>
2024-02-16 21:26:33 -0500

    Added google map

--- src/components/Map.jsx
+++ src/components/Map.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import './Map.css'
+import { useJsApiLoader, GoogleMap } from '@react-google-maps/api';
+
 function Map(){
+    const center = {lat: 40.7128, lng: -74.0060};
+    const API_KEY = import.meta.env.VITE_REACT_GOOGLE_MAPS_KEY;
+    const {isLoaded} = useJsApiLoader({
+        googleMapsApiKey: API_KEY,
+
+    })
+
+    if(!isLoaded){
+        return <div>Loading...</div>
+    }
     return(
-        <div>
-            Map
+        <div id="main-container">
+            <p>Map</p>
+            <div id="map-container">
+                <GoogleMap center={center} zoom={12} mapContainerStyle={{width: '100%', height: '100%'}}>
+
+                </GoogleMap>
+            </div>
         </div>
     )
 }
