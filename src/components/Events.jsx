c41b2916f819aee07eef297a7b6a1415d32f6d77
Zhi Huang Li <zli8362@bths.edu>
2024-02-17 12:15:30 -0500

    Display events

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import './Events.css'
+import LeftBar from './leftBar';
+import ReadPost from './ReadPosts';
+
 function Events(){
     return(
-        <div>
-            Events
+        <div className='event-page'>
+            <LeftBar/>
+            <ReadPost/>
         </div>
     )
 }
