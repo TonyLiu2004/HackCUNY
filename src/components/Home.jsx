cb304ff9e0e0d29ebc97b6d3cede415449db6fa8
Zhi Huang Li <zli8362@bths.edu>
2024-02-18 00:06:16 -0500

    Map Image

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import './Home.css'
-
+import example_map from '../../assets/example-map.png'
 function Home() {
     return (
         <div className="container">
@@ -11,7 +11,7 @@ function Home() {
             </div>
             <div className="content">
                 <p>With the growing issue of self-isolation, especially post-covid, it has become harder and harder for people to connect socially with one another. This problem is especially evident in NYC where urban planning tends to create a much more secluded lifestyle. We aim to help mitigate this problem by creating an easy way for users to find local events to partake in.</p>
-                <img className='image' src="assets\example-map.png"></img>
+                <img className='image' src={example_map}></img>
             </div>
         </div>
     )
