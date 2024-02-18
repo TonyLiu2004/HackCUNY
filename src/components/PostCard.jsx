de06a6866d272535bd0ed592655e4e136e5c4578
Zhi Huang Li <zli8362@bths.edu>
2024-02-17 19:50:08 -0500

    posts css

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import './PostCard.css';
 
 function PostCard({ post }) {
@@ -14,27 +14,15 @@ function PostCard({ post }) {
 
   return (
     <div className="post-card">
-      <div className = "user-content"> {userEmail}</div>
-      <div className = "img-box">
-        <img className='post-image' src={imageURL} alt="Event" />
+      <div className="user-content">{userEmail}</div>
+      <div className="img-box">
+        <img className="post-image" src={imageURL} alt="Event" />
       </div>
-      <div className = "text-content">
-        <span className = "bold">Event: </span> 
-        {event}
-        </div>
-      <div className = "text-content">
-        <span className = "bold">Location: </span> 
-        {location}
-      </div>
-      {eventTime && <div className = "text-content"><span className = "bold">Event Time: </span> {new Date(eventTime).toLocaleString()}</div>}
-      <div className = "text-content">
-        <span className = "bold">Description: </span> 
-        {description}
-      </div>
-      <div className = "text-content">
-        <span className = "bold">Date Posted: </span>
-        {new Date(datePosted).toLocaleString()}
-        </div>
+      <h3 className="text-content">{event}</h3>
+      <div className="text-content">{location}</div>
+      {eventTime && <div className="text-content">Event Time: {new Date(eventTime).toLocaleString()}</div>}
+      <div className="text-content">{description}</div>
+      <div className="text-content">Date Posted: {new Date(datePosted).toLocaleString()}</div>
     </div>
   );
 }
