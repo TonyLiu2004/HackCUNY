31662cfcab4af973c1e52cd381d3fd0b50b9bd2a
Tonyliu2004 <ltsplice@gmail.com>
2024-02-17 19:56:34 -0500

    Revert "Updated css"
    This reverts commit 198c9cd13a46fa90addd834d934e0e75452a8560.

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -14,27 +14,27 @@ function PostCard({ post }) {
 
   return (
     <div className="post-card">
-      <div id="post-title">{event}</div>
-      <div id ="title-box">
-        <div className = "user-content">{new Date(datePosted).toLocaleString()}</div>
-        <div className = "user-content"> {userEmail}</div>
-      </div>
+      <div className = "user-content"> {userEmail}</div>
       <div className = "img-box">
         <img className='post-image' src={imageURL} alt="Event" />
       </div>
+      <div className = "text-content">
+        <span className = "bold">Event: </span> 
+        {event}
+        </div>
       <div className = "text-content">
         <span className = "bold">Location: </span> 
         {location}
       </div>
       {eventTime && <div className = "text-content"><span className = "bold">Event Time: </span> {new Date(eventTime).toLocaleString()}</div>}
       <div className = "text-content">
+        <span className = "bold">Description: </span> 
         {description}
       </div>
-      <h3 className="text-content">{event}</h3>
-      <div className="text-content">{location}</div>
-      {eventTime && <div className="text-content">Event Time: {new Date(eventTime).toLocaleString()}</div>}
-      <div className="text-content">{description}</div>
-      <div className="text-content">Date Posted: {new Date(datePosted).toLocaleString()}</div>
+      <div className = "text-content">
+        <span className = "bold">Date Posted: </span>
+        {new Date(datePosted).toLocaleString()}
+        </div>
     </div>
   );
 }
