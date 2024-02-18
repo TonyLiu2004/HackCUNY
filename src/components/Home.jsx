fca32b5c8842581d3624d97cc8f4c7f47f550e2f
klibths <kli2514@bths.edu>
2024-02-17 21:11:54 -0500

    fixed Home

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import './Home.css'
 
 function Home() {
     return (
-        <p className="container">
+        <div className="container">
             <div className="about-us">
                 <h1>
                     About Us
@@ -13,7 +13,7 @@ function Home() {
                 <p>With the growing issue of self-isolation, especially post-covid, it has become harder and harder for people to connect socially with one another. This problem is especially evident in NYC where urban planning tends to create a much more secluded lifestyle. We aim to help mitigate this problem by creating an easy way for users to find local events to partake in.</p>
                 <img className='image' src="assets\example-map.png"></img>
             </div>
-        </p>
+        </div>
     )
 }
 
