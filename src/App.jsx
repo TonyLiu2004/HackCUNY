987c0844526e2796945e2bca01b2fcb570e4d3fd
Tonyliu2004 <ltsplice@gmail.com>
2024-02-18 09:38:18 -0500

    added createposts for testing

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ import SignIn from './components/SignIn'
 import './App.css';
 import SignUp from './components/Signup';
 import { UserAuth } from './components/UserAuth';
-import CreatePost from './components/CreatePost';
+import CreatePosts from './components/CreatePosts';
 import YourPosts from './components/YourPosts'
 import UpdatePost from './components/UpdatePost';
 import LeftBar from './components/leftBar';
@@ -26,7 +26,7 @@ function App() {
           <Route path="/map" element={<Map />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path='/create-post' element={<CreatePost/>} />
+          <Route path='/create-post' element={<CreatePosts/>} />
           <Route path='/your-posts' element={<YourPosts/>} />
           <Route path='/edit/:id' element={<UpdatePost />} />
           <Route path='/view/:id' element={<ViewPost/>} />
